478fb3374aa1472760d4015f88d0ec73fd606c04
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-11 22:34:28 +0100

    Add DevTools

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import CommentsListContainer from './CommentsListContainer';
+import DevTools from './DevTools';
 
 const App = () => {
-  return <div className="App"><CommentsListContainer/></div>;
+  return <div className="App"><CommentsListContainer/><DevTools /></div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import DevTools from './DevTools';
 import reducer from "./reducer.js";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.js";
 import { addComment } from "./actions.js";
 
-const store = createStore(reducer);
+const store = createStore(reducer, DevTools.instrument());
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -16,5 +17,5 @@ ReactDOM.render(
 	document.getElementById("root")
 );
 
-store.dispatch(addComment("pierwszy komentarz"));
-store.dispatch(addComment("drugi komentarz"));
+store.dispatch(addComment("pierwszy komentarz "));
+store.dispatch(addComment("drugi komentarz "));
